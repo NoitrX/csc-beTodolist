636e7ae79d3ca0f0834c9fe325b4ce990f1b228e
Naufhal <naufhalsimdigxrpl2@gmail.com>
2024-05-31 21:26:34 +0700

    Download Section

--- app/api/todolist_attachment/router.js
+++ app/api/todolist_attachment/router.js
@@ -5,7 +5,7 @@ const { getAll, inputDoc, downloadDoc } = require("./controller");
 const { downloadAttachment } = require("../../services/todolist_attachment");
 
 const upload = require("../../middlewares/multer");
-router.get("/todolist_attachment/:id", authenticateUser, getAll);
+router.get("/todolist_attachment/:id", authenticateUser, authorizeRoles("ADMIN", "USER"), getAll);
 router.get("/download/:id", authenticateUser, authorizeRoles("ADMIN", "USER"), downloadAttachment);
 router.post("/todolist_attachment/create/:id", upload.single("file"), authenticateUser, inputDoc);
 
